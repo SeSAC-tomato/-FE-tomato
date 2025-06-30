410c982f15b6dfadb21ba23b7437f739ebbfb104
ellanelee <ljedoc@naver.com>
2025-06-30 16:17:48 +0900

    :lipstick: 새로운 기능 추가:ButtonComponent추가

---
+++ components/button/LikeButton.tsx
@@ -0,0 +1,22 @@
+"use client"
+import { useState } from "react"
+
+import { LikeButtonProps } from "@/utils/type"
+import HeartIcon from "../icons/HeartIcon"
+
+export default function LikeButton({
+  isLiked = false,
+  fillColor = "#FEE500",
+  handleLike,
+}: LikeButtonProps) {
+  const likedColor = fillColor // red-400
+  const defaultColor = "#CCCCCC" // gray-200
+
+  return (
+    <>
+      <button onClick={handleLike}>
+        <HeartIcon color={isLiked ? likedColor : defaultColor} />
+      </button>
+    </>
+  )
+}

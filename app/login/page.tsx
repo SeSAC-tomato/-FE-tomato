dcd427543e9e92facf41fe7b2c197e5a82225753
namung08 <namung08@gmail.com>
2025-07-03 11:51:54 +0900

    :sparkles: feat: 로그인 상태에 따른 리다이렉트 기능 추가
    - 사용자가 로그인 상태일 경우 메인 화면으로 자동 리다이렉트하는 기능을 추가하였습니다.
    - useAuthStore와 useRouter를 활용하여 로그인 상태를 관리하고 페이지 전환을 구현하였습니다.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,20 @@
 "use client";
 
 import LoginForm from "@/components/auth/LoginForm";
+import { useAuthStore } from "@/store/useAuthStore";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Login() {
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      router.replace("/");
+    }
+  }, [isLoggedIn, router]);
+
   return (
     <div className="min-h-screen bg-white flex flex-col items-center">
       <LoginForm />

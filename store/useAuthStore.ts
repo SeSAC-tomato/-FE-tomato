13152beb0d2f015ea0c736709997edcb8e1b32d8
K1ng1nsu <96197161+K1ng1nsu@users.noreply.github.com>
2025-07-05 21:56:35 +0900

    :sparkles: store 새로고침시에도 유지하게끔 persist 추가 (세션스토리지 사용)

--- store/useAuthStore.ts
+++ store/useAuthStore.ts
@@ -1,11 +1,47 @@
-import { create } from "zustand";
+// import { create } from "zustand";
+//
+// interface User {
+//   email: string;
+//   // 필요하다면 추가 정보
+// }
+//
+// interface AuthState {
+//   user: User | null;
+//   isLoggedIn: boolean;
+//   accessToken: string | null;
+//   isInitialized: boolean;
+//   login: (user: User, accessToken: string) => void;
+//   logout: () => void;
+//   setInitialized: () => void;
+// }
+//
+// export const useAuthStore = create<AuthState>((set) => ({
+//   user: null,
+//   isLoggedIn: false,
+//   accessToken: null,
+//   isInitialized: false,
+//   login: (user, accessToken) => set({ user, isLoggedIn: true, accessToken }),
+//   logout: () => set({ user: null, isLoggedIn: false, accessToken: null }),
+//   setInitialized: () => set({ isInitialized: true }),
+// }));
+
+
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 interface User {
   email: string;
   // 필요하다면 추가 정보
 }
 
+interface TestUser {
+  email: string;
+  userId: number;
+  nickname: string;
+}
+
 interface AuthState {
+  testUser: TestUser | null;
   user: User | null;
   isLoggedIn: boolean;
   accessToken: string | null;
@@ -13,14 +49,26 @@ interface AuthState {
   login: (user: User, accessToken: string) => void;
   logout: () => void;
   setInitialized: () => void;
+  setTestUser: (testUser: TestUser) => void;
 }
 
-export const useAuthStore = create<AuthState>((set) => ({
-  user: null,
-  isLoggedIn: false,
-  accessToken: null,
-  isInitialized: false,
-  login: (user, accessToken) => set({ user, isLoggedIn: true, accessToken }),
-  logout: () => set({ user: null, isLoggedIn: false, accessToken: null }),
-  setInitialized: () => set({ isInitialized: true }),
-}));
+export const useAuthStore = create<AuthState>()(
+    persist(
+        (set) => ({
+          testUser: null,
+          user: null,
+          isLoggedIn: false,
+          accessToken: null,
+          isInitialized: false,
+          login: (user, accessToken) =>
+              set({ user, isLoggedIn: true, accessToken }),
+          logout: () => set({ user: null, isLoggedIn: false, accessToken: null }),
+          setInitialized: () => set({ isInitialized: true }),
+          setTestUser: (testUser) => set({ testUser }),
+        }),
+        {
+          name: 'auth-session-storage', // 세션스토리지에 저장될 key
+          storage: createJSONStorage(() => sessionStorage), // 5.x 버전 방식!
+        }
+    )
+);

03bfbb4d147e7a3c364b230b32d8759f73907f2b
bennett80808 <bennett80808@gmail.com>
2025-07-06 17:05:37 +0900

    :zap: perf api 따로 두고 임포트 하는 방식으로 변경

---
+++ utils/api/user/api.tsx
@@ -0,0 +1,34 @@
+import api from "@/utils/api/axios";
+
+export const removeFromCart = (userId: number, postId: number) => {
+  return api.delete(`/user/${userId}/cart/${postId}`);
+};
+
+export const getCartItems = (userId: number) => {
+  return api.get(`/user/${userId}/cart`);
+};
+
+export const getMyPosts = (userId: number) => {
+  return api.get(`/user/${userId}/posts/myPosts`);
+};
+
+export const changeUserPassword = (
+  userId: number,
+  currentPassword: string,
+  newPassword: string,
+  confirmPassword: string
+) => {
+  return api.put(`/user/${userId}/password`, {
+    currentPassword,
+    newPassword,
+    confirmPassword,
+  });
+};
+
+export const updateUserProfile = (
+  userId: number,
+  nickname: string,
+  address: string
+) => {
+  return api.put(`/user/${userId}/profile`, { nickname, address });
+};

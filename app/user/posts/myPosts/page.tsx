706ca7d56b2134eda694d1c40b2658502194b7d7
bennett80808 <bennett80808@gmail.com>
2025-07-06 16:02:58 +0900

    :adhesive_bandage: refactor 기능 구현중

--- app/user/posts/myPosts/page.tsx
+++ app/user/posts/myPosts/page.tsx
@@ -1,22 +1,26 @@
-'use client'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+"use client";
+import { useEffect, useState } from "react";
+import api from "@/utils/api/axios";
 
-export default function MyPostsPage({ params }: { params: { userId: string } }) {
-    const [posts, setPosts] = useState<any[]>([])
+export default function MyPostsPage({
+  params,
+}: {
+  params: { userId: string };
+}) {
+  const [posts, setPosts] = useState<any[]>([]);
 
-    useEffect(() => {
-        axios.get(`/api/v1/user/${params.userId}/posts/myPosts`).then(res => setPosts(res.data))
-    }, [params.userId])
+  // useEffect(() => {
+  //     axios.get(`/api/v1/user/${params.userId}/posts/myPosts`).then(res => setPosts(res.data))
+  // }, [params.userId])
 
-    return (
-        <div>
-            <h1>내 게시물</h1>
-            {posts.map((post, idx) => (
+  return (
+    <div>
+      <h1>내 게시물</h1>
+      {/* {posts.map((post, idx) => (
                 <div key={idx}>
                     <p>{post.title}</p>
                 </div>
-            ))}
-        </div>
-    )
+            ))} */}
+    </div>
+  );
 }

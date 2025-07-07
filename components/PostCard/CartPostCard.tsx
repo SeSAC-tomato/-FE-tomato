6cf3f2c71ec9c5d14c64f060bd8daa3f0cd146dd
bennett80808 <bennett80808@gmail.com>
2025-07-07 18:30:52 +0900

    :zap: perf 게시글 링크 추가

--- components/PostCard/CartPostCard.tsx
+++ components/PostCard/CartPostCard.tsx
@@ -2,6 +2,8 @@
 "use client";
 import { CartPost } from "@/utils/type/mypage/type";
 import LikeButton from "../button/LikeButton";
+import Link from "next/link";
+import { useParams } from "next/navigation";
 
 interface CartPostCardProps {
   post: CartPost;
@@ -12,31 +14,37 @@ export default function CartPostCard({ post, onUnlike }: CartPostCardProps) {
   // 하트는 항상 채워진 상태
   return (
     <div className="w-[235px] h-[333px] mx-[6px] my-2 flex-shrink-0 bg-white border border-gray-200 rounded-lg shadow flex flex-col justify-start p-2">
-      <div className="w-[210px] h-[216px] ml-1 border border-red-400">
-        {post.img ? (
-          <img
-            src={
-              post.img?.startsWith("http")
-                ? post.img
-                : `http://localhost:8080${post.img}`
-            }
-            alt={post.title}
-            className="w-full h-full object-cover"
-          />
-        ) : (
-          <div className="w-full h-full bg-gray-200 flex items-center justify-center text-gray-500">
-            이미지 없음
-          </div>
-        )}
-      </div>
-      <div className="flex-col justify-start ml-2">
-        <div className="flex items-center">
-          <span>{post.title}</span>
-          <LikeButton isLiked={true} handleLike={onUnlike} />
+      <Link
+        href={`/posts/${post.postId}`}
+        className="block"
+        onClick={() => console.log("이동할 postId:", post.postId)}
+      >
+        <div className="w-[210px] h-[216px] ml-1 border border-red-400">
+          {post.img ? (
+            <img
+              src={
+                post.img?.startsWith("http")
+                  ? post.img
+                  : `http://localhost:8080${post.img}`
+              }
+              alt={post.title}
+              className="w-full h-full object-cover"
+            />
+          ) : (
+            <div className="w-full h-full bg-gray-200 flex items-center justify-center text-gray-500">
+              이미지 없음
+            </div>
+          )}
         </div>
-        <div>{post.price.toLocaleString()}원</div>
-        <div>{formatDate(post.createdAt)}</div>
-      </div>
+        <div className="flex-col justify-start ml-2">
+          <div className="flex items-center">
+            <span>{post.title}</span>
+          </div>
+          <div>{post.price.toLocaleString()}원</div>
+          <div>{formatDate(post.createdAt)}</div>
+        </div>
+      </Link>
+      <LikeButton isLiked={true} handleLike={onUnlike} />
     </div>
   );
 }

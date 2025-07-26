450ba78e795265ba91c6d247560686bca5c06a84
bennett80808 <bennett80808@gmail.com>
2025-07-26 13:49:38 +0900

    :zap: perf 이미지 안보이던거 보이도록 수정

--- components/PostCard/MyPostCard.tsx
+++ components/PostCard/MyPostCard.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { CartPost } from "@/utils/type/mypage/type";
 import Link from "next/link";
-import { formatDate } from "@/utils/domain/label";
+import { formatDate, BASE_URL } from "@/utils/domain/label";
 
 interface MyPostCardProps {
   post: CartPost;
@@ -23,11 +23,21 @@ export default function MyPostCard({ post }: MyPostCardProps) {
       <div className="w-[235px] h-[333px] mx-[6px] my-2 flex-shrink-0 bg-white border border-gray-200 rounded-lg shadow flex flex-col justify-start p-2">
         <div className="w-[210px] h-[216px] ml-1 border border-red-400">
           {post.img ? (
-            <img
-              src={post.img}
-              alt={post.title}
-              className="w-full h-full object-cover"
-            />
+            <>
+              {console.log(
+                "이미지 URL:",
+                `${BASE_URL}/api/v1/post/images/${post.img}`
+              )}
+              <img
+                src={
+                  post.img.startsWith("/api/")
+                    ? `${BASE_URL}${post.img}`
+                    : `${BASE_URL}/api/v1/post/images/${post.img}`
+                }
+                alt={post.title}
+                className="w-full h-full object-cover"
+              />
+            </>
           ) : (
             <div className="w-full h-full bg-gray-200 flex items-center justify-center text-gray-500">
               이미지 없음
